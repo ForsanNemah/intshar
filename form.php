0ea1a83b4f42b745f4f8e977449037b800dc9b3a
ForsanNemah <forsan20172017@gmail.com>
2024-05-20 16:30:08 +0300

    aa

--- form.php
+++ form.php
@@ -217,7 +217,7 @@ echo 'File not found.';
             <img  class="img-fluid" id="loading_gif" src="images/loading.gif" alt="Italian Trulli"  width="50" height="50" >
 
 
-              <button id="form1_button" type="submit" class="btn btn-primary btn-form rounded-pill">تسجيل</button>
+              <button id="form1_button" type="submit" class="btn btn-primary   rounded-pill">تسجيل</button>
             </div>
           </form>
         </div>
--- w_app_form.php
+++ w_app_form.php
@@ -186,7 +186,7 @@ echo 'File not found.';
             <img id="loading2" src="images/loading.gif" alt="Italian Trulli"  width="100" height="100" >
 
 
-              <button id="form2_button" type="submit" class="btn btn-primary btn-form rounded-pill">تسجيل</button>
+              <button id="form2_button" type="submit" class="btn btn-primary   rounded-pill">تسجيل</button>
             </div>
           </form>
         </div>
