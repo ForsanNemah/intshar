539933104f2edd3f19b4bffecd9006ec758b9b99
ForsanNemah <forsan20172017@gmail.com>
2024-05-20 17:41:35 +0300

    aaa

--- parteners.php
+++ parteners.php
@@ -1,16 +1,18 @@
  
 
 
+
  <div class="text-center  mt-4 mb-4">
 
 <h1>
 
-
-شركاء النجاح 
+شركاء النجاح
 
 </h1>
 
 </div
+
+
  
   <section id="parteners">
     <div class="container">
@@ -52,9 +54,9 @@ foreach ($imageFiles as $image) {
     
     
     
-    <div class="col-md-4">
+    <div class="col-md-2 col-sm-6">
     <div class="service">
-      <img src="parteners/'. $image.'" alt="Service 3" class="img-fluid">
+      <img src="parteners/'. $image.'" alt="Service 3" class="img-fluid ">
       <h3> </h3>
       <p> </p>
     </div>
@@ -125,3 +127,14 @@ foreach ($imageFiles as $image) {
 
     </div>
   </section>
+
+
+  <style>
+
+.custom-img {
+  object-fit: cover;
+  width: 100%;
+  height: 100%; /* Set the desired height */
+}
+
+</style>
