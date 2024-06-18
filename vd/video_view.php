a7acf2510fd345d5dcbd53a29f3b1e8ef9b17812
ForsanNemah <forsan20172017@gmail.com>
2024-06-18 20:06:29 +0300

    aa

--- vd/video_view.php
+++ vd/video_view.php
@@ -1,4 +1,4 @@
-<div class="container my-5"  id="table_div">
+<div class="container"  id="table_div">
     <div class="row justify-content-center">
       <div class="col-12 col-md-8 text-center">
        
