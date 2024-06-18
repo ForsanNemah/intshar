a0d8ef83250f84c713dbfe7bc058089dcb51c412
ForsanNemah <forsan20172017@gmail.com>
2024-06-18 05:40:04 +0300

    aa

--- vd/head.php
+++ vd/head.php
@@ -2,16 +2,7 @@
 
 
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-MPQJERPF7F"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-MPQJERPF7F');
-</script>
-
+ 
 
 
 <?php
